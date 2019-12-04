58a399e1caeab1d2f528616f6745af9aff46e9d6
zogodo <742782908@qq.com>
2019-12-04 11:56:36 +0800

    TODO

--- TC1/user_mqtt_client.h
+++ TC1/user_mqtt_client.h
@@ -5,7 +5,7 @@
 #include "mico.h"
 
 #define MQTT_CLIENT_KEEPALIVE   30
-#define MQTT_CLIENT_SUB_TOPIC1   "device/ztc1/set"
+#define MQTT_CLIENT_SUB_TOPIC1  "device/ztc1/set"
 #define MQTT_CLIENT_PUB_TOPIC   "device/ztc1/%s/state"
 #define MQTT_CMD_TIMEOUT        5000  // 5s
 #define MQTT_YIELD_TMIE         5000  // 5s
--- TC1/user_sntp.c
+++ TC1/user_sntp.c
@@ -40,7 +40,8 @@ static void sntp_time_call_back(void)
 
 void sntp_init(void)
 {
-    struct in_addr ipp;ipp.s_addr=0xd248912c;
+    struct in_addr ipp;
+    ipp.s_addr=0xd248912c;
     sntp_set_server_ip_address (0,ipp);
     sntp_start_auto_time_sync (15000,  sntp_time_call_back);        //每小时校准一次
 
